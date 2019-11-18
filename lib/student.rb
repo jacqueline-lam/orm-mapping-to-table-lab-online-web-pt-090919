0f21767f3e7ae617f36b2252a30d8e2dbd6b135f
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-11-18 18:08:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,8 @@ class Student
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0]
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+
   end
   
   # def create
